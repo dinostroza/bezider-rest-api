b6153a7d6009cb8db37829031d420be547c0d23d
dinostroza <dinostrozars@gmail,com>
2017-06-01 19:14:47 -0300

    Create connection to Bezider Postgres/Postgis Database

--- api/settings.py
+++ api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Database Dependencies
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,14 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'beziderdb',
+        'USER': 'bezideradmin',
+        'PASSWORD': 'beziderpass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
