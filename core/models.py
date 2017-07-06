bc9db43e2d207fd853b910d7c7e5d6848fc2c677
dinostroza <dinostrozars@gmail,com>
2017-07-06 18:18:17 -0300

    Add Bookmark and Witness Models and Improving Relational Fields in other models

--- core/models.py
+++ core/models.py
@@ -11,6 +11,9 @@ def newstype_upload_location(instance,filename):
 def news_upload_location(instance,filename):
 	return "news/%s/%s" %(instance.id,filename)
 
+def news_image_upload_location(instance,filename):
+	return "news/%s/%s" %(instance.id,filename)
+
 
 class NewsCategory(models.Model):
 	name          = models.CharField(max_length=64,unique=True,blank=False)
@@ -40,24 +43,113 @@ class NewsType(models.Model):
 		return self.name
 
 class News(models.Model):
-	owner         = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
-	news_type     = models.ForeignKey(NewsType, on_delete=models.PROTECT)
+	user          = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='news', null=True, on_delete=models.SET_NULL)
+	type          = models.ForeignKey(NewsType,related_name='news', on_delete=models.PROTECT)
 	title         = models.CharField(max_length=64,blank=False)
 	content       = models.CharField(max_length=256,blank=True)
 	date          = models.DateTimeField(auto_now=False,auto_now_add=True)
 	location      = models.PointField(default=None,blank=True,null=True)
 	life          = models.IntegerField(default=100)
-	visit_count   = models.IntegerField(default=0)
-	shared_count  = models.IntegerField(default=0)
+	num_visit     = models.IntegerField(default=0)
+	num_share     = models.IntegerField(default=0)
 	is_active     = models.BooleanField(default=True)
+	
+	connections   = models.ManyToManyField(
+			'self'
+		)
+
+	#Social Relation Fields
+	followers     = models.ManyToManyField(
+			settings.AUTH_USER_MODEL,
+			through='social.FollowingNews',
+			through_fields=('news','follower'),
+			related_name='followed_news'
+		)
+
+	likers        = models.ManyToManyField(
+			settings.AUTH_USER_MODEL,
+			through='social.Like',
+			through_fields=('news','user'),
+			related_name='liked_news'
+		)
+
+	witnesses     = models.ManyToManyField(
+			settings.AUTH_USER_MODEL,
+			through='social.Witness',
+			through_fields=('news','user'),
+			related_name='witnessed_news'
+		)
+
+	#captors
+	bookmark_users = models.ManyToManyField(
+			settings.AUTH_USER_MODEL,
+			through='social.Bookmark',
+			through_fields=('news','user'),
+			related_name='bookmarks'
+		)
+
 
 	objects=NewsManager()
 
+	@property
+	def num_followers(self):
+		return self.followers.count()
 
+	@property
+	def follower_username_list(self):
+		usernames = []
+		for obj in self.followers.all():
+			usernames.append(obj.username)
+		return usernames
 
+	@property
+	def follower_id_list(self):
+		ids = []
+		for obj in self.followers.all():
+			ids.append(obj.id)
+		return ids
+
+	@property
+	def num_likes(self):
+		return self.likes.count()
+		#return News.objects.filter(owner=self.id).count()
+
+	@property
+	def liker_username_list(self):
+		usernames = []
+		for obj in self.likers.all():
+			usernames.append(obj.username)
+		return usernames
+
+	@property
+	def liker_id_list(self):
+		ids = []
+		for obj in self.likers.all():
+			ids.append(obj.id)
+		return ids
+
+	@property
+	def num_witnesses(self):
+		return self.witnesses.count()
+		#return News.objects.filter(owner=self.id).count()
+
+	@property
+	def num_comments(self):
+		return self.comments.count()
+
+	@property
+	def num_bookmarks(self):
+		return self.bookmark_users.count()
+
+	@property
+	def num_images(self):
+		return self.images.count()
+
+	@property
 	def latitude(self):
 		return self.location.y
 
+	@property
 	def longitude(self):
 		return self.location.x
 
@@ -69,21 +161,51 @@ class News(models.Model):
 		return self.title
 
 class NewsCatcher(models.Model):
-	owner       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,null=True)
+	user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,null=True,related_name='catchers',related_query_name='catcher')
 	name        = models.CharField(max_length=64,unique=True,blank=False)
 	description = models.CharField(max_length=256,blank=True)
 	area        = models.PolygonField(blank=False)
 	location    = models.PointField(blank=True,null=True)
 	date        = models.DateTimeField(auto_now=False,auto_now_add=True)
+
+	followers   = models.ManyToManyField(
+		settings.AUTH_USER_MODEL,
+		through='social.FollowingCatcher',
+		through_fields=('catcher','follower'),
+		related_name='followed_catcher'
+	)
+	
+	@property
+	def actives_news(self):
+		return News.objects.actives().filter(location__in=self.polygonField)
+		#return News.objects.filter(owner=self.id).count()
+
+	@property
+	def latitude(self):
+		return self.location.y
+
+	@property
+	def longitude(self):
+		return self.location.x
+
 	def __str__(self):
 		return self.name
 
 class NewsImage(models.Model):
-	owner       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,null=True)
-	news        = models.ForeignKey(News, on_delete=models.SET_NULL,null=True)
-	image       = models.ImageField(upload_to=news_upload_location,null=True,blank=True)
+	user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,null=True,related_name='images',related_query_name='image')
+	news        = models.ForeignKey(News, on_delete=models.SET_NULL, null=True, related_name='images', related_query_name='image')
+	image       = models.ImageField(upload_to=news_image_upload_location,null=True,blank=True)
 	date        = models.DateTimeField(auto_now=False,auto_now_add=True)
 	description = models.CharField(max_length=256,blank=True)
 	location    = models.PointField(blank=True,null=True)
+
+	@property
+	def latitude(self):
+		return self.location.y
+
+	@property
+	def longitude(self):
+		return self.location.x
+
 	def __str__(self):
 		return str(self.id)
--- social/models.py
+++ social/models.py
@@ -1,51 +1,102 @@
 from django.contrib.gis.db import models
 from django.conf import settings
 from core.models import News, NewsCatcher
+#from user.models import User
 # Create your models here.
 
 class Comment(models.Model):
-	owner    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	news     = models.ForeignKey(News, on_delete=models.CASCADE)	
+	user    = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments',related_query_name='comment', on_delete=models.CASCADE)
+	news     = models.ForeignKey(News, related_name='comments',related_query_name='comment', on_delete=models.CASCADE)	
 	content  = models.TextField(blank=False)
 	date     = models.DateTimeField(auto_now=False,auto_now_add=True)
 	location = models.PointField(default=None,blank=True,null=True)
+
+	@property
+	def latitude(self):
+		return self.location.y
+
+	@property
+	def longitude(self):
+		return self.location.x
+
+	class Meta:
+		verbose_name = 'Comment'
+		verbose_name_plural = 'Comments'
 	def __str__(self):
 		return '%s comment on \"%s\"' %(self.owner.username,self.news.title)
 
-class Comment2Comment(models.Model):
-	owner    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	comment  = models.ForeignKey(Comment, on_delete=models.CASCADE)	
-	content  = models.TextField(blank=False)
-	date     = models.DateTimeField(auto_now=False,auto_now_add=True)
-	location = models.PointField(default=None,blank=True,null=True)
+class Reply(models.Model):
+	user      = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='replies',related_query_name='reply', on_delete=models.CASCADE)
+	comment    = models.ForeignKey(Comment, related_name='replies',related_query_name='reply',on_delete=models.CASCADE)
+	content    = models.TextField(blank=False)
+	date       = models.DateTimeField(auto_now=False,auto_now_add=True)
+	location   = models.PointField(default=None,blank=True,null=True)
+
+	@property
+	def latitude(self):
+		return self.location.y
+
+	@property
+	def longitude(self):
+		return self.location.x
+
+	class Meta:
+		verbose_name = 'Reply'
+		verbose_name_plural = 'Replies'
 	def __str__(self):
 		return '%s comment to %s on \"%s\"' %(self.owner.username,self.comment.owner.username, self.comment.news.title)
 
+class Bookmark(models.Model):
+	user    = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='bookmark_data',related_query_name='bookmark_data', on_delete=models.CASCADE)
+	news    = models.ForeignKey('core.News', related_name='bookmark_data',related_query_name='bookmark_data',on_delete=models.CASCADE)
+
 class Like(models.Model):
-	user    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	news    = models.ForeignKey(News, on_delete=models.CASCADE)
+	user    = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='likes',related_query_name='like', on_delete=models.CASCADE)
+	news    = models.ForeignKey(News,related_name='likes',related_query_name='like', on_delete=models.CASCADE)
+	class Meta:
+		verbose_name = 'Like'
+		verbose_name_plural = 'Likes'
 	def __str__(self):
 		return '%s likes \"%s\"' %(self.user.username, self.news.title)
 
+class Witness(models.Model):
+	user  = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='witnesses_data',related_query_name='witness_data',on_delete=models.CASCADE)
+	news  = models.ForeignKey(News,related_name='witnesses_data',related_query_name='witness_data', on_delete=models.CASCADE)
+	class Meta:
+		verbose_name = 'Witness'
+		verbose_name_plural = 'Witnesses'
+	def __str__(self):
+		return '%s witness \"%s\"' %(self.user.username, self.news.title)
+
 class FollowingUser(models.Model):
-	follower    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='follower')
-	followed    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='followed')
+	follower    = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='follower_users_data',related_query_name='follower_user_data', on_delete=models.CASCADE)
+	user        = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='followed_users_data',related_query_name='followed_user_data', on_delete=models.CASCADE)
 	date        = models.DateTimeField(auto_now=False,auto_now_add=True)
 	min         = models.IntegerField(default=0)
+	class Meta:
+		verbose_name = 'Following User'
+		verbose_name_plural = 'Following Users'
 	def __str__(self):
 		return '%s is following to \"%s\"' %(self.follower.username, self.followed.username)
 
 class FollowingNews(models.Model):
-	follower    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	news        = models.ForeignKey(News, on_delete=models.CASCADE)	
+	follower    = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='following_news_data',related_query_name='following_news_data', on_delete=models.CASCADE)
+	news        = models.ForeignKey(News,related_name='following_news_data',related_query_name='following_news_data', on_delete=models.CASCADE)	
 	date        = models.DateTimeField(auto_now=False,auto_now_add=True)
+	class Meta:
+		verbose_name = 'Following News'
+		verbose_name_plural = 'Following News'
 	def __str__(self):
 		return '%s is following \"%s\"' %(self.follower.username, self.news.title)
 
 class FollowingCatcher(models.Model):
-	follower    = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	catcher     = models.ForeignKey(NewsCatcher, on_delete=models.CASCADE)	
+	follower    = models.ForeignKey(settings.AUTH_USER_MODEL,related_name='following_catcher_data',related_query_name='following_catcher_data', on_delete=models.CASCADE)
+	catcher     = models.ForeignKey(NewsCatcher,related_name='following_catcher_data',related_query_name='following_catcher_data', on_delete=models.CASCADE)	
 	date        = models.DateTimeField(auto_now=False,auto_now_add=True)
 	min         = models.IntegerField(default=0)
+	class Meta:
+		verbose_name = 'Following Catcher'
+		verbose_name_plural = 'Following Catchers'
 	def __str__(self):
 		return '%s is following \"%s\"' %(self.follower.username, self.catcher.name)
+		
